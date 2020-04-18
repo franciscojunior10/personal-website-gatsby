0160329f1943156062482fe3c65144c5c64e15b6
Francisco Junior <franciscojn49@gmail.com>
2020-04-18 18:23:24 -0300

    feat: added image perfil and social networks

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
     'gatsby-plugin-sass',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import { Link } from 'gatsby';
+
+import {
+  Link,
+  StaticQuery,
+  graphql,
+} from 'gatsby';
+
+import Img from 'gatsby-image';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  faInstagram,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 
 import Layout from '../components/layout';
 import SEO from '../components/SEO';
@@ -10,8 +25,54 @@ const IndexPage = () => (
       description="full-stack developer, developing apps"
       keywords="ReactJS, React Native, Node, Gatsby, Front-end, Back-end, API"
     />
-    <h1 className="title has-text-centered">Francisco Júnior</h1>
-    <h2 className="subtitle has-text-centered">Full Stack Developer</h2>
+    <figure className="perfil image is-128x128">
+      <StaticQuery
+        query={graphql`
+        query {
+          file(relativePath: { eq: "perfil.jpg" }) {
+            childImageSharp {
+              fixed(width: 128, height: 128) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      `}
+        render={(data) => (
+          <Img
+            fixed={data.file.childImageSharp.fixed}
+            imgStyle={{ borderRadius: '100%' }}
+            alt="Francisco Júnior"
+          />
+        )}
+      />
+    </figure>
+
+    <div className="has-text-centered">
+
+      <h1 className="title">Francisco Júnior</h1>
+      <h2 className="subtitle">Full Stack Developer</h2>
+
+      <a href="https://github.com/franciscojunior10" aria-label="github">
+        <span className="icon is-large fa-2x">
+          <FontAwesomeIcon icon={faGithub} />
+        </span>
+      </a>
+
+      <a href="https://www.linkedin.com/in/franciscojunior10/" aria-label="linkedin">
+        <span className="icon is-large fa-2x">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </span>
+      </a>
+
+      <a href="https://instagram.com/junniorcarvallho" aria-label="instagram">
+        <span className="icon is-large fa-2x">
+          <FontAwesomeIcon icon={faInstagram} />
+        </span>
+      </a>
+
+    </div>
+
     <section className="section content is-size-4-desktop is-size-5-touch">
       <h4 className="title">Hello, I am Francisco das Chagas dos Anjos Carvalho Júnior</h4>
       <p>
