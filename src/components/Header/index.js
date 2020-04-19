1c58e607f305c7085bb85724b9fb198e05c6dadb
Francisco Junior <franciscojn49@gmail.com>
2020-04-18 23:28:04 -0300

    feat: created nav bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,35 +1,84 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-);
+import './styles.scss';
+
+class Header extends Component {
+    state = {
+      isMenuOpen: false,
+    }
+
+    handleMenu = () => {
+      this.setState((previousState) => ({
+        isMenuOpen: !previousState.isMenuOpen,
+      }));
+    }
+
+    closeMenu = () => {
+      this.setState({ isMenuOpen: false });
+    }
+
+    render = () => {
+      const { siteTitle } = this.props;
+      const { isMenuOpen } = this.state;
+      const burguerClass = isMenuOpen ? 'is-active' : '';
+
+      return (
+        <nav className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <Link className="navbar-item is-size-4" to="/">
+                {siteTitle}
+              </Link>
+              <button
+                className={`navbar-burger ${burguerClass}`}
+                type="button"
+                aria-label="menu"
+                onClick={this.handleMenu}
+              >
+                <span aria-hidden="true" />
+                <span aria-hidden="true" />
+                <span aria-hidden="true" />
+              </button>
+            </div>
+            <div className={`navbar-menu ${burguerClass}`}>
+              <div className="navbar-end">
+                <Link
+                  className="navbar-item is-size-5"
+                  to="/"
+                  onClick={this.closeMenu}
+                >
+                  About
+                </Link>
+                <Link
+                  className="navbar-item is-size-5"
+                  to="/blog"
+                  onClick={this.closeMenu}
+                >
+                  Blog
+                </Link>
+                <Link
+                  className="navbar-item is-size-5"
+                  to="/projects"
+                  onClick={this.closeMenu}
+                >
+                  Projects
+                </Link>
+                <Link
+                  className="navbar-item is-size-5"
+                  to="/stack"
+                  onClick={this.closeMenu}
+                >
+                  Stack
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+      );
+    }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
